e34e565f077f0075017db83e15c8f21e6562d17a
Dario <darioarias12@gmail.com>
2022-09-21 23:24:57 -0400

    serves as a master holding a reference to all of the stocks the app supports. Updating this sequence automatically adds support, provided that the data is valid.

---
+++ dynamic/stock_data.py
@@ -0,0 +1,9 @@
+supported_stock = {
+  'select': 'Select a Stock',
+  'AAPL': "Apple",
+  'MSFT': "Microsoft",
+  'AMZN': "Amazon",
+  'GOOGL': "Alphabet Inc Class A (Google)",  
+  'TSLA': "Tesla",
+  'PLTR': 'Palantir Technologies'
+}
