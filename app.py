16e05500343c4fce06e4236d9a4c3ba6ba0a226a
Dario <darioarias12@gmail.com>
2022-09-21 23:20:49 -0400

    update app to use callback and session_state to call on the model to predict stock data.

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
+from ast import arg
 import streamlit as st
-# import mymodel as m
+import bentoml
+from dynamic import stock_on_change, supported_stock, stock_name_format
 
-st.write('HELLO WORLD!!!???')
+# Interaction Part
+st.markdown("# Interactions")
+
+option = st.selectbox (
+  'Which Stock would you like to learn more about?',
+  supported_stock,
+  key="stock",
+  on_change=stock_on_change,
+  args=[st.session_state],
+  format_func=stock_name_format
+)
+
+if st.session_state.get('prediction', None):
+  st.write(f'You want to learn about: {option}')
+  st.write(st.session_state['prediction'])
+  del st.session_state['prediction']
+
+
