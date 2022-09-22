bc91e928f9cda40b58c7da02513524afc601aab0
Dario <darioarias12@gmail.com>
2022-09-21 23:23:45 -0400

    makes available functionalities needed for the model to transform features. This is the place we can add other functionalities as they come up

---
+++ dynamic/ml_support.py
@@ -0,0 +1,16 @@
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import numpy as np
+
+scaler = MinMaxScaler(feature_range=(0,1))
+features = ['Open', 'High', 'Low', 'Volume']
+
+with open("CSV/MSFT_kaggle.csv", mode='r', encoding='utf-8') as data:
+  msft_data = pd.read_csv(data, na_values=['null'], index_col='Date', parse_dates=True, infer_datetime_format=True)
+  scaler.fit(msft_data[['Open', 'High', 'Low', 'Volume']])
+
+def normalize(point, scaler=scaler):
+  transformed = scaler.transform([point])
+  feature_transform = pd.DataFrame(columns=features, data=transformed)
+  nparr = np.array(feature_transform)
+  return nparr.reshape(feature_transform.shape[0], 1, feature_transform.shape[1])
