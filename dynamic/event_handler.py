25b035b75961ac5b9386eab471ebfe90745482c3
Dario <darioarias12@gmail.com>
2022-09-23 16:58:38 -0400

    update to use production version of the lsmt model, not the local version

--- dynamic/event_handler.py
+++ dynamic/event_handler.py
@@ -1,7 +1,8 @@
 import json
 import yfinance as yf
+import requests
 from .stock_data import supported_stock
-from . import model
+from . import BASE_URL
 from .ml_support import normalize
 
 def query_features(ticker, *features, testing = False, file=""):
@@ -24,10 +25,19 @@ def stock_on_change(state):
   features_label = ["open", "dayHigh", "dayLow", "volume", "currentPrice"]
   features = query_features(state['stock'], *features_label)
   
-  prediction = model.predict(normalize(features[:-1]))[0][0]
+
+  # prediction = model.predict(normalize(features[:-1]))[0][0]
+
+  # prediction = model.predict(normalize(features[:-1]))[0][0]
+  data = requests.post(
+    BASE_URL,
+    headers={"content-type": "application/json"}, 
+    data=f"{[[[n for n in normalize(features[:-1])[0][0]]]]}"
+  ).json()
   
   # features_label.append('prediction')
-  features.append(float(prediction))
+  features.append(float(data[0][0]))
+  # print("DATRA", data)
   
   # result = {}
   # for label, val in zip(features_label, features):
