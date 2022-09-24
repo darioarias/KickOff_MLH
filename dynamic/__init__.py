929c1a92c9ea07eb22957fb04894e16d24cf35e0
Dario <darioarias12@gmail.com>
2022-09-23 21:58:32 -0400

    new link for model

--- dynamic/__init__.py
+++ dynamic/__init__.py
@@ -1,4 +1,4 @@
 import bentoml
 # model = bentoml.keras.load_model('lstm_finance:latest')
-BASE_URL = "https://lstm-predict.run-us-west2.goorm.io/predict"
+BASE_URL = "https://lstm-predictor.run-us-west2.goorm.io/predict" 
 from .event_handler import *
